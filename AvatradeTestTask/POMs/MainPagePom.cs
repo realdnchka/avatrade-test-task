56c968445de866f7d09990a779a1f50d1b29d925
Danil Chernov <realdnchka@gmail.com>
2023-10-24 21:21:42 +0400

    add mainpage pom

--- AvatradeTestTask/POMs/MainPagePom.cs
+++ AvatradeTestTask/POMs/MainPagePom.cs
@@ -5,7 +5,26 @@ namespace AvatradeTestTask.POMs;
 
 public class MainPagePom: BasePom
 {
-    public MainPagePom(WebDriver driver) : base(driver)
+    public MainPagePom(WebDriver driver) : base(driver) {}
+    
+    //web elements
+    private By _registrationButton = By.XPath("//a[contains(text(), 'Register now')]");
+    private By _emailInput = By.XPath("");
+    private By _passwordInput = By.XPath("");
+    
+    //methods
+    public void RegistrationButtonClick()
     {
+        Click(_registrationButton);
+    }
+
+    public void EmailInputSendKeys(string text)
+    {
+        SendKeys(_emailInput, text);
+    }
+
+    public void PasswordInputSendKeys(string text)
+    {
+        SendKeys(_passwordInput, text);
     }
 }
